9a1cb638bd1de47c834060d3997e7475414d32aa
Darshan Reddy <darshanreddy26@gmail.com>
2025-07-09 08:17:08 -0400

    added spaces

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_quote, name='add_quote'),
+    path('edit/<int:quote_id>/', views.edit_quote, name='edit_quote'),
+    path('delete/<int:quote_id>/', views.delete_quote, name='delete_quote'),
+    path('tag/<int:tag_id>/', views.quotes_by_tag, name='quotes_by_tag'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,26 +1,62 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Quote, Tag
-# Import models to get data from DB
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
-    quotes = Quote.objects.all()    # Fetch all quotes
-    tags = Tag.objects.all()        # Fetch all tags
-    return render(request, 'home.html', {'quotes': quotes, 'tags': tags})
-from django.shortcuts import render, redirect
-from .models import Quote, Tag
-
-def add_quote(request):
+    quotes = Quote.objects.all()
     tags = Tag.objects.all()
+    return render(request, 'home.html', {'quotes': quotes, 'tags': tags})
 
+
+def quotes_by_tag(request, tag_id):
+    quotes = Quote.objects.filter(tag_id=tag_id)
+    tags = Tag.objects.all()
+    return render(request, 'home.html', {'quotes': quotes, 'tags': tags})
+
+
+@login_required
+def add_quote(request):
     if request.method == 'POST':
         text = request.POST['text']
         author = request.POST['author']
         tag_id = request.POST['tag']
-        tag = Tag.objects.get(id=tag_id)
+        Quote.objects.create(text=text, author=author, tag_id=tag_id)
+        return redirect('home')
+    tags = Tag.objects.all()
+    return render(request, 'add.html', {'tags': tags})
 
-        # Save the new quote
-        Quote.objects.create(text=text, author=author, tag=tag)
 
-        return redirect('home')  # Redirect to homepage after saving
+@login_required
+def edit_quote(request, quote_id):
+    quote = get_object_or_404(Quote, id=quote_id)
+    if request.method == 'POST':
+        quote.text = request.POST['text']
+        quote.author = request.POST['author']
+        quote.tag_id = request.POST['tag']
+        quote.save()
+        return redirect('home')
+    tags = Tag.objects.all()
+    return render(request, 'edit.html', {'quote': quote, 'tags': tags})
 
-    return render(request, 'add_quote.html', {'tags': tags})
+
+@login_required
+def delete_quote(request, quote_id):
+    quote = get_object_or_404(Quote, id=quote_id)
+    quote.delete()
+    return redirect('home')
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Quote, Tag
+
+# Other functions ...
+
+def edit_quote(request, quote_id):
+    quote = get_object_or_404(Quote, id=quote_id)
+    if request.method == 'POST':
+        quote.text = request.POST['text']
+        quote.author = request.POST['author']
+        quote.tag_id = request.POST['tag']
+        quote.save()
+        return redirect('home')
+    tags = Tag.objects.all()
+    return render(request, 'edit.html', {'quote': quote, 'tags': tags})
